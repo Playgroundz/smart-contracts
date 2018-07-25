a1047ae16766aaefa941b3cbfc57b827287ca7f3
philz99 <dlim@pm-games.com>
2018-07-25 14:35:38 -0700

    Added Pausable.sol

--- IOGToken.sol
+++ IOGToken.sol
@@ -4,15 +4,15 @@ import "./SafeMath.sol";
 import "./Ownable.sol";
 import "./StandardToken.sol";
 
-contract IOGToken is StandardToken, Ownable {
+contract IOGToken is StandardToken, Ownable, Pausable {
 
     // events
     event Burn(address indexed burner, uint256 amount);
     event AddressLocked(address indexed _owner, uint256 _expiry);
 
     // ERC20 constants
-    string public constant name = "IIOToken";
-    string public constant symbol = "IIO";
+    string public constant name = "IOXToken";
+    string public constant symbol = "IOX";
     uint8 public constant decimals = 18;
     uint256 public constant TOTAL_SUPPLY = 120 * 1000 * (10 ** uint256(decimals));
 
@@ -20,17 +20,14 @@ contract IOGToken is StandardToken, Ownable {
     mapping (address => uint256) public addressLocks;
 
     // constructor
-    function IOGToken() public {
+    constructor() public {
         totalSupply_ = TOTAL_SUPPLY;
         balances[msg.sender] = TOTAL_SUPPLY;
         emit Transfer(0x0, msg.sender, TOTAL_SUPPLY);
-
-        // distribution
-        initialPoolDistribution();
     }
 
     // distribution
-    function initialPoolDistribution() onlyOwner internal {
+    function distribution() onlyOwner internal {
         // early inverstors pool (locked)
         uint256 earlyInverstorPoolToken = 7777 * (10 ** uint256(decimals));
         address earlyInverstorPoolWallet = 0xC0531e14a0B2E91C2ca646a35f5b619184339A53;
@@ -63,13 +60,25 @@ contract IOGToken is StandardToken, Ownable {
         return (now >= addressLocks[_sender]);
     }
 
-    // transfer
-    function transfer(address _to, uint256 _value) canTransfer(msg.sender) public returns (bool success) {
+    // ERC20 Methods
+    function transfer(address _to, uint256 _value) canTransfer(msg.sender) whenNotPaused public returns (bool success) {
         return super.transfer(_to, _value);
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) canTransfer(_from) public returns (bool success) {
+    function transferFrom(address _from, address _to, uint256 _value) canTransfer(_from) whenNotPaused public returns (bool success) {
         return super.transferFrom(_from, _to, _value);
+    }
+    
+    function approve(address _spender, uint256 _value) whenNotPaused public returns (bool) {
+        return super.approve(_spender, _value);
+    }
+
+    function increaseApproval(address _spender, uint _addedValue) whenNotPaused public returns (bool success) {
+        return super.increaseApproval(_spender, _addedValue);
+    }
+
+    function decreaseApproval(address _spender, uint _subtractedValue) whenNotPaused public returns (bool success) {
+        return super.decreaseApproval(_spender, _subtractedValue);
     }
 
     // burn
@@ -85,5 +94,11 @@ contract IOGToken is StandardToken, Ownable {
 
         emit Burn(_who, _value);
         emit Transfer(_who, address(0), _value);
+    }
+	
+	// Token Drain
+    function emergencyERC20Drain(ERC20 token, uint256 amount) external onlyOwner {
+        // owner can drain tokens that are sent here by mistake
+        token.transfer(owner, amount);
     }
 }
