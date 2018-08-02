bc91d4c14027cff97377450759a6d19477f540ee
limdp <dlim@pm-games.com>
2018-08-01 17:05:47 -0700

    Removed unused files

--- contracts/IOGToken.sol
+++ contracts/IOGToken.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.4.21;
 
-import "./SafeMath.sol";
-import "./Ownable.sol";
-import "./StandardToken.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "openzeppelin-solidity/contracts/lifecycle/Pausable.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
 
 contract IOGToken is StandardToken, Ownable, Pausable {
 
@@ -10,9 +11,9 @@ contract IOGToken is StandardToken, Ownable, Pausable {
     event Burn(address indexed burner, uint256 amount);
     event AddressLocked(address indexed _owner, uint256 _expiry);
 
-    // ERC20 constants
-    string public constant name = "IOXToken";
-    string public constant symbol = "IOX";
+    // erc20 constants
+    string public constant name = "IOX4Token";
+    string public constant symbol = "IOX4";
     uint8 public constant decimals = 18;
     uint256 public constant TOTAL_SUPPLY = 120 * 1000 * (10 ** uint256(decimals));
 
@@ -24,25 +25,6 @@ contract IOGToken is StandardToken, Ownable, Pausable {
         totalSupply_ = TOTAL_SUPPLY;
         balances[msg.sender] = TOTAL_SUPPLY;
         emit Transfer(0x0, msg.sender, TOTAL_SUPPLY);
-    }
-
-    // distribution
-    function distribution() onlyOwner internal {
-        // early inverstors pool (locked)
-        uint256 earlyInverstorPoolToken = 7777 * (10 ** uint256(decimals));
-        address earlyInverstorPoolWallet = 0xC0531e14a0B2E91C2ca646a35f5b619184339A53;
-        addressLocks[earlyInverstorPoolWallet] = now + 10 minutes;
-        transfer(earlyInverstorPoolWallet, earlyInverstorPoolToken);        
-        emit AddressLocked(earlyInverstorPoolWallet, earlyInverstorPoolToken);
-
-        // private sale pool
-        uint256 privateSalePoolToken = 88888 * (10 ** uint256(decimals));
-        address privateSalePoollWallet = 0xEcA254594c5bBCCEBc321e9252cc886cE37Be914;
-        transfer(privateSalePoollWallet, privateSalePoolToken);
-
-        // team reserved pool (locked)
-
-        // playgroundz ecosystem pool
     }
 
     // lock
@@ -60,7 +42,13 @@ contract IOGToken is StandardToken, Ownable, Pausable {
         return (now >= addressLocks[_sender]);
     }
 
-    // ERC20 Methods
+    function timeLock(address _to, uint256 _value, uint256 releaseDate) onlyOwner public {
+        addressLocks[_to] = releaseDate;
+        transfer(_to, _value);
+        emit AddressLocked(_to, _value);
+    }
+
+    // erc20 methods
     function transfer(address _to, uint256 _value) canTransfer(msg.sender) whenNotPaused public returns (bool success) {
         return super.transfer(_to, _value);
     }
@@ -96,7 +84,7 @@ contract IOGToken is StandardToken, Ownable, Pausable {
         emit Transfer(_who, address(0), _value);
     }
 	
-	// Token Drain
+	// token drain
     function emergencyERC20Drain(ERC20 token, uint256 amount) external onlyOwner {
         // owner can drain tokens that are sent here by mistake
         token.transfer(owner, amount);
--- truffle.js
+++ truffle.js
@@ -1,16 +1,22 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "dust mountain work view talent farm reduce ketchup only fringe bullet bright";
+var mnemonic = "siren mystery tortoise oil front leg mistake artwork reform sunny lazy put";
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
 networks: {
+  development: {
+    host: "localhost",
+    port: 8545,
+    network_id: "*", // Match any network id,
+    gas: 2990000
+  },
   ropsten: {
     provider: function() { 
      return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/1b5251d8c8114f0ca0e3187e53db027b") 
     },
     network_id: 3,
-    gas: 670000,
+    gas: 2990000,
    }
    }
 };
